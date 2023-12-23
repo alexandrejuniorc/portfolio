1496bdbabe83b6662b39f9da93f429a0de6fec93
Alexandre <alejunior.dev@gmail.com>
2023-12-23 10:15:47 -0300

    feat: :sparkles: create providers to project

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+
+export default function ThemesProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
